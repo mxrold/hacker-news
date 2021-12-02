eab88ff73465bb67a450b554d3566fee1d1e6dd3
Martin <martin.davrold@gmail.com>
2021-12-02 16:24:21 -0300

    :heavy_plus_sign: [Installed] react-router-dom. Structure of the website routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home, Favorites, NotFound } from '../pages'
+
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/favorites' element={<Favorites />} />
+        <Route exact path='/notfound' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
